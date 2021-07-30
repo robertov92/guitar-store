16ff84b4f2b896de15405aac09c488b8e30ce72b
robertov92 <vil13004@byui.edu>
2021-07-30 14:00:25 -0500

    add product functionality complete

--- controllers/account.js
+++ controllers/account.js
@@ -1,3 +1,7 @@
+const { validationResult } = require('express-validator/check');
+
+const Product = require('../models/product');
+
 exports.getAccount = (req, res, next) => {
     res.render('pages/account', {});
 }
@@ -13,3 +17,52 @@ exports.getAccInfo = (req, res, next) => {
 exports.getWishList = (req, res, next) => {
     res.render('pages/wishlist', {});
 }
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('pages/add-product', {
+        editing: false,
+        errorMessage: null,
+        validationErrors: [],
+        hasError: false
+    });
+}
+
+exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title;
+    const stock = req.body.stock;
+    const brand = req.body.brand;
+    const category = req.body.category;
+    const price = req.body.price;
+    const imageUrl = req.body.imageUrl;
+    const description = req.body.description;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('pages/add-product', {
+            editing: false,
+            hasError: true,
+            product: { title, stock, brand, category, imageUrl, price, description },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
+
+    const product = new Product({
+        title: title,
+        stock: stock,
+        brand: brand,
+        category: category,
+        price: price,
+        description: description,
+        imageUrl: imageUrl,
+        //userId: req.user
+    });
+
+    product.save()
+        .then(() => {
+            res.redirect('/account');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,6 @@
 const routes = require('express').Router();
+const { check, body } = require('express-validator/check');
+
 const accountControllers = require('../controllers/account');
 
 routes.get('/login', accountControllers.getLogin);
@@ -6,6 +8,18 @@ routes.get('/login', accountControllers.getLogin);
 routes.get('/info', accountControllers.getAccInfo);
 
 routes.get('/wishlist', accountControllers.getWishList);
+
+routes.get('/add-product', accountControllers.getAddProduct);
+
+routes.post('/add-product', [
+    body('title').isString().isLength({ min: 3 }).trim(),
+    body('stock').isInt(),
+    body('brand').isString().isLength({ min: 3 }).trim(),
+    body('category').isString().isLength({ min: 3 }).trim(),
+    body('price').isFloat(),
+    body('imageUrl').isURL(),
+    body('description').isLength({ min: 8, max: 400 }).trim()
+], accountControllers.postAddProduct);
 
 routes.get('/', accountControllers.getAccount);
 
