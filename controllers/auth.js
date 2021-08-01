6f8edfda43c9dee28bde11d354a04c68bad58e8d
robertov92 <vil13004@byui.edu>
2021-07-31 22:58:34 -0500

    middleware added

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,5 +53,66 @@ exports.postSignup = (req, res, next) => {
 };
 
 exports.getLogin = (req, res, next) => {
-    res.render('pages/login', {});
-}
+    res.render('pages/login', {
+        errorMessage: req.flash('error'),
+        oldInput: { email: '', password: '' },
+        validationErrors: []
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('pages/login', {
+            //pageTitle: 'Login',
+            errorMessage: errors.array()[0].msg,
+            oldInput: { email: email, password: password },
+            validationErrors: errors.array()
+        });
+    }
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                return res.status(422).render('pages/login', {
+                    //pageTitle: 'Login',
+                    errorMessage: 'Invalid email',
+                    oldInput: { email: email, password: password },
+                    validationErrors: []
+                });
+            }
+            bcrypt.compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true; // sets isLoggedIn to true
+                        req.session.user = user;
+                        // saves the user into the session
+                        return req.session.save(err => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    return res.status(422).render('pages/login', {
+                        //pageTitle: 'Login',
+                        errorMessage: 'Invalid password',
+                        oldInput: { email: email, password: password },
+                        validationErrors: []
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.redirect('/project1/login');
+                });
+        })
+        .catch(err => {
+            console.log(err);
+            res.redirect('/project1/500');
+        });
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,10 @@ const routes = require('express').Router();
 const { check, body } = require('express-validator/check');
 
 const authControllers = require('../controllers/auth');
+const alreadyLogedIn = require('../middleware/alreadyLogedIn');
 const User = require('../models/user');
 
-routes.get('/signup', authControllers.getSignup);
+routes.get('/signup', alreadyLogedIn, authControllers.getSignup);
 
 routes.post('/signup', [
     check('email').isEmail().withMessage('Please enter a valid email').custom((value, { req }) => {
@@ -24,6 +25,13 @@ routes.post('/signup', [
     })
 ], authControllers.postSignup);
 
-routes.get('/login', authControllers.getLogin);
+routes.get('/login', alreadyLogedIn, authControllers.getLogin);
+
+routes.post('/login', [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password', 'Please enter a password with only numbers and text, and at least 4 characters long').isLength({ min: 4 }).isAlphanumeric()
+], authControllers.postLogin);
+
+routes.post('/logout', authControllers.postLogout);
 
 module.exports = routes;
